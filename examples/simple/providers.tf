369d81b5f28e010184af50ab83ea4684000fac57
skankipati11 <surya.kankipati11@gmail.com>
2026-02-04 12:12:10 +0530

    first commit

--- examples/simple/providers.tf
+++ examples/simple/providers.tf
@@ -1,3 +1,15 @@
-provider "snowflake" {}
+provider "snowflake" {
+
+  organization_name = "xodjopc"
+  account_name      = "jtc04659"
+  user              = "SKANKIPATI"
+  role              = "ACCOUNTADMIN"
+  authenticator     = "SNOWFLAKE_JWT"
+  private_key       = var.snowflake_private_key
+
+  preview_features_enabled = [
+    "snowflake_stage_resource",
+  ]
+}
 
 provider "context" {}
