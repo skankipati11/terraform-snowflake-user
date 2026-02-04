9b29ecbe7047fe49bb8aa233187cd54bc2b627b3
skankipati11 <surya.kankipati11@gmail.com>
2026-02-04 12:20:06 +0530

    first commit

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -2,3 +2,9 @@ variable "context_templates" {
   description = "A map of context templates to use for generating user names"
   type        = map(string)
 }
+
+variable "snowflake_private_key" {
+  type        = string
+  description = "Private key used to access Snowflake"
+  sensitive   = true
+}
--- variables.tf
+++ variables.tf
@@ -215,3 +215,9 @@ variable "context_templates" {
   type        = map(string)
   default     = {}
 }
+
+variable "snowflake_private_key" {
+  type        = string
+  description = "Private key used to access Snowflake"
+  sensitive   = true
+}
